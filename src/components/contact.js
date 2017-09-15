c4480b10c4b8f9403d110b04fa2f736e0b7a3beb
Dino Trojak <dinotrojak@gmail.com>
2017-09-15 17:20:31 +0200

    changed contant info

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,14 +10,10 @@ class Contact extends React.Component {
         <div onClick={this.props.toggleContact} className="closeClick"></div>
         <div className="content">
           <p>
-            SDDS is a Zagreb based design studio founded by Sandro Dujmenovic, working in the fields of arts, culture and commerce.
-            The studio works within a wide range of projects, from luxurious branding to heavy duty visual identities and user experience projects. Upcoming projects include custom type design for Museum of Louvre, total design for Unitas, Croatian thread company, user interface and experience for Interfilm,
-            Croatian film production company.<br/><br/>
-            For full portfolio or a collaboration please contact via e-mail.<br/><br/>
-
-            <a href="mailto:i@sandrosandro.com">i@sandrosandro.com</a><br/>
-            +385 (0)98 586 589
-
+            Beton is a Zagreb based design agency founded by Sandro Dujmenović, Marko Mlinarić and Sanja Nikolić, working in the fields of arts, culture and commerce. The studio works within a wide range of projects, from luxurious branding to heavy duty visual identities and user experience projects. Upcoming projects include custom type design for Museum of Louvre, total design for Unitas, Croatian thread company, user interface and experience for Interfilm, Croatian film production company.
+            <br/><br/>For full portfolio or a collaboration please contact via e-mail.
+            <br/><a href="mailto:i@sandrosandro.com">hi@beton.com.hr</a>
+            <br/><br/>M. +385 (0)91 307 2569
           </p>
         </div>
       </div>
